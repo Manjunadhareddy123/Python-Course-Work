a39042a112200989ce25d3c6f5de30d9d369858e
Manjunadhareddy123 <manjunadhareddy62@gmail.com>
2025-07-08 16:22:30 +0530

    all

---
+++ classwork/list.py
@@ -0,0 +1,13 @@
+# List
+li=[]
+print(li) # []
+my_list2=list()
+print(my_list2) # []
+n=[1,2,3,4,5]
+n # [1, 2, 3, 4, 5]
+friuts=['apple','banana','cherry']
+friuts # ['apple', 'banana', 'cherry']
+li=[10,20.5,2+3j,True,'manju',"nadha"]
+li  # [10, 20.5, (2+3j), True, 'manju', 'nadha']
+nested_list=[[1,2,3],['a','b','c'],["A","B","C"]]
+nested_list # [[1, 2, 3], ['a', 'b', 'c'], ['A', 'B', 'C']]
